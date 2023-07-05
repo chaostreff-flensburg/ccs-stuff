68f517eaeffe6eb7a12cd7ee95c82c9f2d5a04eb
scammo <sbrinkmann@54gradsoftware.de>
2023-07-05 14:56:04 +0200

    csv adressen stuff hinzugefügt

---
+++ post-versand-adressen/js-parse-script/index.js
@@ -0,0 +1,94 @@
+const fs = require("fs");
+const csvParser = require("csv-parser");
+const { stringify } = require("csv-stringify");
+
+
+const writableStream = fs.createWriteStream('../hochschul-addressen_alle_sortiert_studierende.csv');
+
+const importColumns = [
+  "nr",
+  "Hochschulkurzname",
+  "Hochschulname",
+  "Hochschultyp",
+  "Trägerschaft",
+  "Bundesland",
+  "AnzahlStudierende",
+  "Gründungsjahr",
+  "Promotionsrecht",
+  "Habilitationsrecht",
+  "Straße",
+  "Postleitzahl",
+  "Ort",
+  "Postfach",
+  "Postleitzahl",
+  "Ort",
+  "Telefonvorwahl",
+  "Telefon",
+  "Fax",
+  "HomePage",
+  "MitgliedHRK"
+];
+const exportColumns = [
+  "NAME",
+  "ZUSATZ",
+  "STRASSE",
+  "NUMMER",
+  "PLZ",
+  "STADT",
+  "LAND",
+  "ADRESS_TYP",
+  "REFERENZ"
+];
+
+const stringifier = stringify({ header: true, delimiter: ';', columns: exportColumns });
+
+let result = [];
+
+fs.createReadStream("../hochschul-addressen-raw.csv")
+  .pipe(csvParser({ encoding: null }))
+  .on("data", (data) => {
+    console.log('====================')
+    console.log(data)
+    if (data["Trägerschaft"] === 'öffentlich-rechtlich') {
+      const [street, houseNumber] = seperateHouseNumberStreet(data["Straße"])
+      const row = {
+        "NAME": data['Hochschulname'],
+        "ZUSATZ": "",
+        "STRASSE": street,
+        "NUMMER": houseNumber,
+        "PLZ": data['Postleitzahl (Postanschrift)'],
+        "STADT": data['Ort (Postanschrift)'],
+        "LAND": "DEU",
+        "ADRESS_TYP": "HOUSE",
+        "REFERENZ": data['Anzahl Studierende'], //"CCS.2023.1",
+      }
+      result.push(row)
+    }
+  })
+  .on("end", () => {
+    result = result.sort((a, b) => a["REFERENZ"] - b["REFERENZ"]);
+    result.unshift({
+      "NAME": "Chaostreff Flensburg e.V.",
+      "ZUSATZ": "",
+      "STRASSE": "Apenrader Str.",
+      "NUMMER": "49",
+      "PLZ": "24939",
+      "STADT": "Flensburg",
+      "LAND": "DEU",
+      "ADRESS_TYP": "HOUSE",
+      "REFERENZ": "Abbsender"
+    })
+    console.log('result', result[2])
+    result.forEach((row) => {
+      stringifier.write(row);
+    })
+    stringifier.pipe(writableStream);
+    console.log("Finished writing data");
+  });
+
+function seperateHouseNumberStreet(streetAndNumber) {
+  const streetAndNumberArray = streetAndNumber.split(' ')
+  const houseNumber = streetAndNumberArray[streetAndNumberArray.length - 1]
+  const street = streetAndNumberArray.slice(0, streetAndNumberArray.length - 1).join(' ')
+  return [street, houseNumber]
+}
